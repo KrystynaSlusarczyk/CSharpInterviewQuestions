b3efa72b1b35734049f8d50c6543953c25a5f77f
CybransLoyalist <cybransloyalist@gmail.com>
2020-10-05 18:29:03 +0200

    adding Testability module

---
+++ CSharpInterviewQuestions/CSharpInterviewQuestions/Testability.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpInterviewQuestions
+{
+    public class UntestableMedicalScoreCalculator
+    {
+        public double CalculateAverageMedicalScoreOfPeople()
+        {
+            var repository = new Repository();
+            var allPeople = repository.GetAllPeopleFromDatabase();
+            var scores = allPeople.Select(p => StaticMedicalScoreCalculator.GetScore(p));
+
+            return scores.Average();
+        }
+    }
+
+    public class TestableMedicalScoreCalculator
+    {
+        private readonly IRepository _repository;
+        private readonly IMedicalScoreCalculator _medicalDataAnalyser;
+
+        public TestableMedicalScoreCalculator(IRepository repository, IMedicalScoreCalculator medicalDataAnalyser)
+        {
+            _repository = repository;
+            _medicalDataAnalyser = medicalDataAnalyser;
+        }
+
+        public double CalculateAverageMedicalScoreOfPeople()
+        {
+            var allPeople = _repository.GetAllPeopleFromDatabase();
+            var scores = allPeople.Select(p => _medicalDataAnalyser.GetScore(p));
+
+            return scores.Average();
+        }
+    }
+
+    public static class StaticMedicalScoreCalculator
+    {
+        private static Random _random = new Random();
+        public static int GetScore(Person p)
+        {
+            //imagine it performs some complex calculations basing on personal data
+            return _random.Next(1, 101);
+        }
+    }
+
+    public interface IMedicalScoreCalculator
+    {
+        int GetScore(Person p);
+    }
+    public class MedicalScoreCalculator : IMedicalScoreCalculator
+    {
+        private Random _random = new Random();
+        public int GetScore(Person p)
+        {
+            //imagine it performs some complex calculations basing on personal data
+            return _random.Next(1, 101);
+        }
+    }
+    
+    public interface IRepository
+    {
+        List<Person> GetAllPeopleFromDatabase();
+    }
+
+    public class Repository : IRepository
+    {
+        public List<Person> GetAllPeopleFromDatabase()
+        {
+            //imagine we connect to a database here and retun real data
+            return new List<Person>
+            {
+            };
+        }
+    }
+
+    public class Person
+    {
+        private string _name;
+
+        public Person(string name)
+        {
+            _name = name;
+        }
+    }
+}
---
+++ CSharpInterviewQuestions/CSharpInterviewQuestionsTests/MedicalScoreCalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿
+using CSharpInterviewQuestions;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CSharpInterviewQuestionsTests
+{
+    [TestFixture]
+    public class MedicalScoreCalculatorTests
+    {
+        private Mock<IRepository> _repositoryMock;
+        private Mock<IMedicalScoreCalculator> _medicalScoreCalculatorMock;
+        private TestableMedicalScoreCalculator _cut;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _repositoryMock = new Mock<IRepository>();
+            _medicalScoreCalculatorMock = new Mock<IMedicalScoreCalculator>();
+            _cut = new TestableMedicalScoreCalculator(_repositoryMock.Object, _medicalScoreCalculatorMock.Object);
+        }
+
+        [Test]
+        public void IfThereAre3PeopleWithScores_10_20_60_TheAverageScoreShouldBe_30()
+        {
+            var people = new List<Person>
+                {
+                    new Person("John Smith"),
+                    new Person("John Doe"),
+                    new Person("Jane Doe")
+                };
+
+            _repositoryMock.Setup(a => a.GetAllPeopleFromDatabase()).Returns(
+                people);
+
+            _medicalScoreCalculatorMock.Setup(a => a.GetScore(people[0])).Returns(10);
+            _medicalScoreCalculatorMock.Setup(a => a.GetScore(people[1])).Returns(20);
+            _medicalScoreCalculatorMock.Setup(a => a.GetScore(people[2])).Returns(60);
+
+            Assert.AreEqual(30, _cut.CalculateAverageMedicalScoreOfPeople());
+        }
+    }
+}
