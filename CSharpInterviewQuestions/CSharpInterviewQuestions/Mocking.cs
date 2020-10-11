a39d2d3c2c3347df4afa59a191fe581d49e5bf2c
CybransLoyalist <cybransloyalist@gmail.com>
2020-10-11 18:24:12 +0200

    Mocking in UserController

---
+++ CSharpInterviewQuestions/CSharpInterviewQuestions/Mocking.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInterviewQuestions
+{
+    public class UserController
+    {
+        private readonly IUsersRepository _repository;
+
+        public UserController(IUsersRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<User> GetAllActive()
+        {
+            return _repository.GetAll().Where(a => a.IsActive);
+        }
+    }
+
+    public interface IUsersRepository //implementation would connect to some actual database
+    {
+        IEnumerable<User> GetAll();
+    }
+
+    public class User : IEquatable<User>
+    {
+        public int Id { get; }
+        public string Email { get; }
+        public bool IsActive { get; }
+
+        public User(int id, string email, bool isActive = false)
+        {
+            Id = id;
+            Email = email;
+            IsActive = isActive;
+        }
+
+        public bool Equals(User other)
+        {
+            return Id == other.Id;
+        }
+    }
+}
---
+++ CSharpInterviewQuestions/CSharpInterviewQuestionsTests/UserControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using CSharpInterviewQuestions;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CSharpInterviewQuestionsTests
+{
+    [TestFixture]
+    public class UserControllerTests
+    {
+        private UserController _cut;
+        private Mock<IUsersRepository> _usersRepositoryMock;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            // _cut = new UserController(new RealDbRepository()); //bad idea!
+            _usersRepositoryMock = new Mock<IUsersRepository>();
+            _cut = new UserController(_usersRepositoryMock.Object);
+        }
+
+        [Test]
+        public void GetAllActive_ShouldExcludeNonActiveUsers()
+        {
+            var allUsers = new List<User>
+            {
+                new User(1, "user1@gmail.com", false),
+                new User(2, "user2@gmail.com", true),
+                new User(3, "user3@gmail.com", false),
+            };
+
+            _usersRepositoryMock.Setup(a => a.GetAll()).Returns(allUsers);
+
+            var result = _cut.GetAllActive();
+            var expectedResult = new List<User>
+            {
+                new User(2, "user2@gmail.com", true)
+            };
+
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+    }
+}
